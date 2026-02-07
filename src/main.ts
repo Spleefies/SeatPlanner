8b459a49edd20d0512e1764184506a92a48b90ca
Spleefies <cutecatty32@gmail.com>
2026-02-07 14:55:54 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import './style.css'
+import Sortable, { Swap } from 'sortablejs';
+
+const params = new URLSearchParams(window.location.search);
+const itemsParam = params.get('items')
+
+const names = itemsParam ? JSON.parse(itemsParam) : [];
+
+
+for (let i = 0; i < 30; i++) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  cell.textContent = names[i];
+  cell.draggable = true;
+
+  document.querySelector<HTMLDivElement>('#app')!.appendChild(cell);
+}
+
+Sortable.mount(new Swap());
+Sortable.create(document.querySelector<HTMLDivElement>('#app')!, {
+  swap: true,
+  animation: 150,
+})
