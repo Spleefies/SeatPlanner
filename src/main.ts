e3858878db8c0fa29287a7bdb0f93176cce1b79e
Spleefies <cutecatty32@gmail.com>
2026-02-08 17:39:52 +0800

    link shortener?

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,19 @@ import Sortable, { Swap } from 'sortablejs';
 const params = new URLSearchParams(window.location.search);
 const itemsParam = params.get('items')
 
-const names = itemsParam ? JSON.parse(itemsParam) : [];
+const appDiv = document.querySelector<HTMLDivElement>('#app')!;
+const names = itemsParam ? itemsParam.split(',') : [];
 
 for (let i = 0; i < 30; i++) {
   const cell = document.createElement("div");
   cell.className = "cell";
   cell.textContent = names[i];
 
-  document.querySelector<HTMLDivElement>('#app')!.appendChild(cell);
+  appDiv.appendChild(cell);
 }
 
 Sortable.mount(new Swap());
-Sortable.create(document.querySelector<HTMLDivElement>('#app')!, {
+Sortable.create(appDiv, {
   swap: true,
   animation: 250,
 })
