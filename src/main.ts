29a7dd7bfff5491a5e8ea3d8acc9c3d6ce1ee128
Spleefies <cutecatty32@gmail.com>
2026-02-21 12:25:16 +0800

    lil bit late

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,24 @@ const itemsParam = params.get('items')
 const appDiv = document.querySelector<HTMLDivElement>('#app')!
 const names = itemsParam ? itemsParam.split(',') : []
 let newNames = names
+const originalIndexes = [...Array(30).keys()].map(String)
+let newIndexes = [...originalIndexes]
 
 for (let i = 0; i < 30; i++) {
+  const name = names[i]
   const cell = document.createElement("div")
   cell.className = "cell"
-  cell.textContent = names[i]
-  cell.dataset.originalIndex = String(i)
-
+  
+  if (name && isDigit(name[1])) {
+    cell.dataset.originalIndex = name.substring(0,2)
+    cell.classList.add("changed")
+    cell.textContent = name.substring(2)
+    newNames[i] = name.substring(2)
+    newIndexes[i] = name.substring(0,2)
+  } else {    
+    cell.textContent = name
+    cell.dataset.originalIndex = String(i)
+  }
   appDiv.appendChild(cell)
 }
 
@@ -24,19 +35,52 @@ Sortable.create(appDiv, {
   onSort(evt){
     const dragged = evt.item
     const swapped = evt.swapItem
-
+    
     if (swapped) {
       if (evt.newIndex!=dragged.dataset.originalIndex)
         dragged.classList.add("changed")
       else dragged.classList.remove("changed")
-
+      
       if (evt.oldIndex!=swapped.dataset.originalIndex)
         swapped.classList.add("changed")
       else swapped.classList.remove("changed")
     }
     if (evt.oldIndex && evt.newIndex) {
-      [newNames[evt.oldIndex],newNames[evt.newIndex]] = [newNames[evt.newIndex],newNames[evt.oldIndex]]
+      [newNames[evt.oldIndex],newNames[evt.newIndex]] = [newNames[evt.newIndex],newNames[evt.oldIndex]];
+      [newIndexes[evt.oldIndex],newIndexes[evt.newIndex]] = [newIndexes[evt.newIndex],newIndexes[evt.oldIndex]]
     }
   }
 })
+const setDefaultBtn = document.querySelector<HTMLButtonElement>("#setDefault")
+setDefaultBtn?.addEventListener("click", setDefault)
 
+function setDefault() {
+  const cells = document.querySelectorAll<HTMLButtonElement>(".cell")
+  
+  for (let i = 0; i < 30; i++) {
+    const cell = cells[i]
+    cell.dataset.originalIndex = String(i)
+    cell.classList.remove("changed")
+  }
+
+  newIndexes = [...originalIndexes]
+}
+
+const copyBtn = document.querySelector<HTMLButtonElement>("#copy")
+copyBtn?.addEventListener("click", copyURL)
+
+function copyURL() {
+  let items = ''
+  for (let i = 0; i < 30; i++) {
+    if (newIndexes[i] != originalIndexes[i]) {
+      items += newIndexes[i].padStart(2,'0')
+    }
+    items += newNames[i]
+    items += ','
+  }
+  navigator.clipboard.writeText(`${window.location.href.split('?')[0]}?items=${items}`.replace(' ','+'))
+  copyBtn!.textContent = "Copied!"
+  setTimeout(() => {copyBtn!.textContent = "Copy Link"}, 1000)
+}
+
+function isDigit(char: string) { return /\d/.test(char) }
